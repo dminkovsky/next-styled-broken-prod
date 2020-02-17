528b1bdbcd606f41f9735a431c096c74f74523b8
Dmitry Minkovsky <dminkovsky@gmail.com>
2020-02-17 13:13:41 -0500

    first

---
+++ pages/_document.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import _Document from 'next/document';
+import {ServerStyleSheet} from 'styled-components';
+
+export default class Document extends _Document {
+    static async getInitialProps(ctx) {
+        const sheet = new ServerStyleSheet();
+        const originalRenderPage = ctx.renderPage;
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: App => props =>
+                        sheet.collectStyles(<App {...props} />),
+                });
+            const initialProps = await _Document.getInitialProps(
+                ctx,
+            );
+            return {
+                ...initialProps,
+                styles: (
+                    <>
+                        {initialProps.styles}
+                        {sheet.getStyleElement()}
+                    </>
+                ),
+            };
+        } finally {
+            sheet.seal();
+        }
+    }
+}
